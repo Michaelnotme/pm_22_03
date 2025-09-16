4bc42f15f1eea5dd7123b317ead510acd8111841
Michaelnotme <mishabos2103@gmail.com>
2025-09-16 19:13:04 +0300

    Raw Gulp page?

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-// gulpfile.js
-
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require ('gulp-cssnano');
+const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 
 // HTML
@@ -10,11 +10,18 @@ function htmlTask() {
         .pipe(dest('dist/html'));
 }
 
+function indexTask() {
+    return src('app/index.html')
+        .pipe(dest('dist'));
+}
+
 // SCSS
 function scssTask() {
     return src('app/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(dest('dist/css'));
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream());
 }
 
 // JS
@@ -25,9 +32,18 @@ function jsTask() {
 
 // Images
 function imgTask() {
-    return src('app/img/**/*')
+    return src('app/img/**/*', { encoding: false })
         .pipe(imagemin())
         .pipe(dest('dist/img'));
 }
+// Reloading
+function serve() {
+    browserSync.init( { server: { baseDir: 'dist' } });
+    watch('app/html/**/*.html', htmlTask).on('change', browserSync.reload);
+    watch('app/scss/**/*.scss', scssTask);
+    watch('app/js/**/*.js', jsTask).on('change', browserSync.reload);
+    watch('app/img/**/*', imgTask).on('change', browserSync.reload);
+    watch('app/index.html', indexTask).on('change', browserSync.reload);
+}
 
-exports.default = parallel(htmlTask, scssTask, jsTask, imgTask);
+exports.default = parallel(htmlTask, indexTask, scssTask, jsTask, imgTask, serve);
