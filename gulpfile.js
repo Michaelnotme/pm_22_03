72686a3c9b36490beb1b49027cb57292cd1ba992
Michaelnotme <mishabos2103@gmail.com>
2025-10-16 08:52:17 +0300

    Third Lab Finished

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const fileInclude = require('gulp-file-include'); // підключаємо
+const rename = require('gulp-rename');
 
 // HTML
 function htmlTask() {
@@ -19,6 +20,11 @@ function scssTask() {
     return src('src/app/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cssnano())
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min',
+            extname: '.css'
+        }))
         .pipe(dest('dist/css', { sourcemaps: '.' }))
         .pipe(browserSync.stream());
 }
